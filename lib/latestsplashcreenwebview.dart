93f4381d7651ad93bfbf4b4af567970e99d0df67
Rahul <rahulsingh73679@gmail.com>
2023-01-26 10:40:12 +0530

    my 4th commit

--- lib/latestsplashcreenwebview.dart
+++ lib/latestsplashcreenwebview.dart
@@ -1,3 +1,4 @@
+/*
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -99,3 +100,4 @@ class _WebViewExampleState extends State<WebViewExample> {
     );
   }
 }
+*/
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 4), () {
+    Timer(Duration(seconds: 5), () {
       Navigator.pushReplacement(
           context,
           MaterialPageRoute(
