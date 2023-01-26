7927dcd459f18096065dfd4136aa4518169a888e
Rahul <rahulsingh73679@gmail.com>
2023-01-26 08:46:57 +0530

    splashscreen add karna tha

--- lib/latestsplashcreenwebview.dart
+++ lib/latestsplashcreenwebview.dart
@@ -34,7 +34,7 @@ class _SplashScreenState extends State<SplashScreen> {
       child: Container(
         height: 1100,
         width: 2100,
-        child: Image.asset('assets/todo12.jfif'),
+        child: Image.asset('assets/launch2.jfif'),
       ),
     ));
   }
@@ -59,7 +59,7 @@ class _WebViewExampleState extends State<WebViewExample> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('To-do List'),
+        title: const Text('Calculator'),
         // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
         actions: <Widget>[],
       ),
@@ -67,7 +67,7 @@ class _WebViewExampleState extends State<WebViewExample> {
       // to allow calling Scaffold.of(context) so we can show a snackbar.
       body: Builder(builder: (BuildContext context) {
         return WebView(
-          initialUrl: "https://r1pns.github.io/r/",
+          initialUrl: " https://rahulsingh73679.github.io/Scientic-Calculator/",
           javascriptMode: JavascriptMode.unrestricted,
           onWebViewCreated: (WebViewController webViewController) {
             _controller.complete(webViewController);
@@ -99,15 +99,3 @@ class _WebViewExampleState extends State<WebViewExample> {
     );
   }
 }
-
-
-// Gaya College of Engineering   https://www.gcegaya.ac.in
-// flutter build apk --build-name=1.0.3 --build-number=3
-  
-//  flutter pub get
-//  flutter pub run flutter_launcher_icons:main
-
-//  https://r1pns.github.io/r/
-  // angellist message
-  
-// I am not sure whether I will get this job or not but I am 100% sure about one thing that I can do my very best to complete the task I am assigned with.
